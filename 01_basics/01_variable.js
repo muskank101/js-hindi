38ddc6d41ef5da8ac7920a4e889552593d4ebdc7
Muskan Koshti <103520140+muskank101@users.noreply.github.com>
2024-02-18 15:32:39 +0000

    var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const NAME = "Muskan"
+let newVar = "Hello"
+var email = "muskank1399@gmail.com"
+
+jadu = "didn't defined"
+nummmm = 9627456
+let donno ;
+console.table([NAME,newVar , email, donno,jadu])
+console.log(typeof(jadu))
+console.log(typeof(nummmm))
+console.log(typeof(donno))
